0180da4934e2309d3c12e29bfa51a503195e48dd
Sergei Ivchenko <sergey.ivchenko89@yandex.ru>
2021-10-29 16:51:41 +0300

    Add input file checking

--- index.php
+++ index.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 use SergeiIvchenko\CommissionTask\App;
 
-require __DIR__.'/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 $inputFilePath = realpath($argv[1]);
-$outputFilePath = dirname(__FILE__) . '/'. $argv[2];
+$outputFilePath = dirname(__FILE__) . '/' . $argv[2];
 
-(new App())->run($inputFilePath, $outputFilePath);
+if (false === $inputFilePath) {
+    var_dump(sprintf('Ошибка чтения входного файла %s!\r\n', $argv[1]));
+} else {
+    (new App())->run($inputFilePath, $outputFilePath);
+}
+
